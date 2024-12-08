6232f111c539c82f68afb064e28bf5af7f6c7d40
Ailana Karamchakova <karamchakova.ae@phystech.edu>
2024-12-08 17:09:58 +0300

    add CustomArrayList

---
+++ CustomArrayList/src/main/java/org/example/CustomArrayList.java
@@ -0,0 +1,64 @@
+package org.example;
+
+/**
+ * Кастомная реализация ArrayList.
+ *
+ * @param <A> Тип элементов, которые будут храниться в списке.
+ */
+public class CustomArrayList<A> implements CustomArrayListMethods<Object> {
+  /**
+   * list - структура, в которой хранятся все элементы.
+   * size - поле, в котором храниться информация о количестве элементов в массиве на данный момент.
+   * capacity - поле, в котором содержится информация о максимальном количестве элементов в массиве на данный момент.
+   */
+  private Object[] list;
+  private int size;
+  private final int capacity = 5;
+
+  /**
+   * Конструктор инициализирует новый объект CustomArrayList.
+   */
+  public CustomArrayList() {
+    list = new Object[capacity];
+  }
+
+  @Override
+  public void add(Object item) {
+    if (item == null) {
+      throw new NullPointerException("Element cannot be null");
+    }
+    if (size + 1 >= capacity) {
+      increaseCapacity();
+    }
+    list[size++] = item;
+  }
+
+  @Override
+  public Object get(int index) {
+    if (index < 0 || index >= size) {
+      throw new ArrayIndexOutOfBoundsException("Element with index " + index + " does not exist");
+    }
+    return list[index];
+  }
+
+  @Override
+  public void remove(int index) {
+    for (int i = index; i < size; i++) {
+      list[i] = list[i + 1];
+    }
+
+    size--;
+  }
+
+  /**
+   * Увеличивает capacity CapacityArrayList.
+   */
+  private void increaseCapacity() {
+    int newCapacity = capacity * 2;
+    Object[] newList = new Object[newCapacity];
+    for (int i = 0; i < capacity; i++) {
+      newList[i] = list[i];
+    }
+    list = newList.clone();
+  }
+}
---
+++ CustomArrayList/src/main/java/org/example/CustomArrayListMethods.java
@@ -0,0 +1,34 @@
+package org.example;
+
+/**
+ * Интерфейс для кастомной реализации ArrayList.
+ *
+ * @param <A> Тип элементов, которые будут храниться в списке.
+ */
+public interface CustomArrayListMethods<A> {
+  /**
+   * Добавляет элемент в конец списка.
+   *
+   * @param item Элемент, который нужно добавить.
+   * @throws NullPointerException Если элемент равен null
+   */
+  void add(A item);
+
+  /**
+   * Возвращает элемент по указанному индексу.
+   *
+   * @param index Индекс элемента, который нужно вернуть.
+   * @return Элемент по указанному индексу.
+   * @throws ArrayIndexOutOfBoundsException Если индекс выходит за пределы списка.
+   */
+  A get(int index);
+
+  /**
+   * Удаляет элемент по указанному индексу.
+   *
+   * @param index Индекс элемента, который нужно удалить.
+   * @throws ArrayIndexOutOfBoundsException если индекс выходит за пределы списка.
+   */
+  void remove(int index);
+}
+
